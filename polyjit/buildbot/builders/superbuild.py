becc428b863ee0d8072f950702e8ca64646427cf
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-06-08 13:54:42 +0200

    superbuild: do not build inside uchroot

--- polyjit/buildbot/builders/superbuild.py
+++ polyjit/buildbot/builders/superbuild.py
@@ -2,8 +2,8 @@ import sys
 
 from polyjit.buildbot.builders import register
 from polyjit.buildbot import slaves
-from polyjit.buildbot.utils import (builder, define, git, ucmd, ucompile,
-                                    s_sbranch, s_force, s_trigger)
+from polyjit.buildbot.utils import (builder, define, git, cmd, compile,
+                                    s_sbranch, s_force, s_trigger, mkdir)
 from polyjit.buildbot.repos import make_cb, make_new_cb, codebases
 from buildbot.plugins import util
 from buildbot.changes import filter
@@ -19,24 +19,22 @@ accepted_builders = slaves.get_hostlist(slaves.infosun)
 # yapf: disable
 def configure(c):
     steps = [
-        define("UCHROOT_SRC_ROOT", "/mnt/build/polli-sb"),
-        define("UCHROOT_POLLI_BUILDDIR", "/mnt/build"),
         git('polli-sb', 'master', codebases, workdir="build/polli-sb"),
-        ucmd('cmake', P("UCHROOT_SRC_ROOT"),
-             '-DCMAKE_BUILD_TYPE=Release',
-             '-DBUILD_SHARED_LIBS=On',
-             '-G', 'Ninja',
-             env={
-                 "PATH": "/opt/cmake/bin:/usr/local/bin:/usr/bin:/bin"
-             },
-             name="cmake",
-             description="[uchroot] cmake: release build",
-             descriptionDone="[uchroot] configured."),
-        ucompile("ninja", "-C", P("UCHROOT_POLLI_BUILDDIR"),
-                 haltOnFailure=True, name="build jit",
-                 description="[uchroot] building PolyJIT",
-                 descriptionDone="[uchroot] built PolyJIT",
-                 timeout=4800)
+        cmd('cmake', "build/polli-sb",
+            '-DCMAKE_BUILD_TYPE=Release',
+            '-DBUILD_SHARED_LIBS=On',
+            '-G', 'Ninja',
+            env={
+                "PATH": "/opt/cmake/bin:/usr/local/bin:/usr/bin:/bin"
+            },
+            name="cmake",
+            description="[uchroot] cmake: release build",
+            descriptionDone="[uchroot] configured."),
+        compile("ninja",
+                haltOnFailure=True, name="build jit",
+                description="[uchroot] building PolyJIT",
+                descriptionDone="[uchroot] built PolyJIT",
+                timeout=4800)
     ]
 
     c['builders'].append(builder("polyjit-superbuild", None, accepted_builders,
