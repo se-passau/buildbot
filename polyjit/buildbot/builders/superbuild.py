fec9024330de30d74045e2da19f08026529913b9
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-06-26 12:23:46 +0200

    superbuild: uchroot assisted superbuild of polyjit&llvm

--- polyjit/buildbot/builders/superbuild.py
+++ polyjit/buildbot/builders/superbuild.py
@@ -2,9 +2,12 @@ import sys
 
 from polyjit.buildbot.builders import register
 from polyjit.buildbot import slaves
-from polyjit.buildbot.utils import (builder, define, git, cmd, compile,
-                                    s_nightly, s_sbranch, s_force, s_trigger)
+from polyjit.buildbot.utils import (builder, define, git,
+                                    cmd, ucmd, ucompile, ip,
+                                    s_nightly, s_sbranch, s_force, s_trigger,
+                                    hash_upload_to_master)
 from polyjit.buildbot.repos import make_cb, make_new_cb, codebases
+from polyjit.buildbot.master import URL
 from buildbot.plugins import util
 from buildbot.changes import filter
 
@@ -19,27 +22,44 @@ accepted_builders = slaves.get_hostlist(slaves.infosun)
 # yapf: disable
 def configure(c):
     steps = [
-        git('polli-sb', 'master', codebases, workdir="build/polli-sb"),
-        cmd('cmake', "./polli-sb",
-            '-DCMAKE_BUILD_TYPE=Release',
-            '-DCMAKE_INSTALL_PREFIX=./',
-            '-DBUILD_SHARED_LIBS=On',
-            '-G', 'Ninja',
-            env={
-                "PATH": "/opt/cmake/bin:/usr/local/bin:/usr/bin:/bin"
-            },
-            name="cmake",
-            description="[uchroot] cmake: release build",
-            descriptionDone="[uchroot] configured."),
-        compile("ninja",
-                haltOnFailure=True, name="build jit",
-                description="[uchroot] building PolyJIT",
-                descriptionDone="[uchroot] built PolyJIT",
-                timeout=4800)
+        define("SUPERBUILD_ROOT", ip("%(prop:builddir)s/polli-sb")),
+        define("UCHROOT_SUPERBUILD_ROOT", "/mnt/polli-sb"),
+        # FIXME Testing:
+        define("POLYJIT_DEFAULT_BRANCH", "WIP-merge-next"),
+
+        git('polli-sb', 'master', codebases, workdir=P("SUPERBUILD_ROOT")),
+        ucmd('cmake', P("UCHROOT_SUPERBUILD_ROOT"),
+             '-DCMAKE_BUILD_TYPE=Release',
+             '-DCMAKE_INSTALL_PREFIX=./_install',
+             ip('-DPOLYJIT_BRANCH_CLANG=%(prop:POLYJIT_DEFAULT_BRANCH)'),
+             ip('-DPOLYJIT_BRANCH_LLVM=%(prop:POLYJIT_DEFAULT_BRANCH)'),
+             ip('-DPOLYJIT_BRANCH_POLLI=%(prop:POLYJIT_DEFAULT_BRANCH)'),
+             ip('-DPOLYJIT_BRANCH_POLLY=%(prop:POLYJIT_DEFAULT_BRANCH)'),
+             '-G', 'Ninja',
+             env={
+                 "PATH": "/opt/cmake/bin:/usr/local/bin:/usr/bin:/bin"
+             },
+             name="cmake",
+             description="[uchroot] cmake: release build",
+             descriptionDone="[uchroot] configured."),
+        ucompile("ninja", "install",
+                 haltOnFailure=True, name="build jit",
+                 description="[uchroot] building PolyJIT",
+                 descriptionDone="[uchroot] built PolyJIT",
+                 timeout=4800),
+        cmd("tar", "czf", "../polyjit_sb.tar.gz", "-C", "./_install", ".")
     ]
 
-    c['builders'].append(builder("polyjit-superbuild", None, accepted_builders,
-                         tags=['polyjit'], factory=BuildFactory(steps)))
+    upload_pj = hash_upload_to_master(
+        "polyjit_sb.tar.gz",
+        "../polyjit_sb.tar.gz",
+        "public_html/polyjit_sb.tar.gz", URL)
+    steps.extend(upload_pj)
+
+    c['builders'].append(
+        builder("polyjit-superbuild", None,
+                accepted_builders,
+                tags=['polyjit'], factory=BuildFactory(steps)))
 # yapf: enable
 
 
