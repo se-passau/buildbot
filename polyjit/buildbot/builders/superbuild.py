55440b9e4bad66dc2ae562e1ee1276dde6524ccb
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-06-26 17:21:29 +0200

    interpolate as late as possible

--- polyjit/buildbot/builders/superbuild.py
+++ polyjit/buildbot/builders/superbuild.py
@@ -45,7 +45,7 @@ def configure(c):
                  "PATH": "/opt/cmake/bin:/usr/local/bin:/usr/bin:/bin"
              },
              mounts={
-                 ip('%(prop:cmake_prefix)s') : "/opt/cmake"
+                 '%(prop:cmake_prefix)s' : "/opt/cmake"
              },
              name="cmake",
              description="[uchroot] cmake: release build",
