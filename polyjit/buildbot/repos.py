b3dc1359f0e164ceb01e008b4770e440082d5e09
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-06-28 13:40:02 +0200

    polyjit: make gitpoller aware of new codebase dict.
    The new codebases dict is only used as a fallback, if we cannot fill out
    the ChangeSource object with enough data from the repo. Therefore, keys
    like 'branch' only allow a single string as argument (a default value).
    
    Everything else can be configured as usual on the scheduler/git-poller side.

--- polyjit/buildbot/repos.py
+++ polyjit/buildbot/repos.py
@@ -4,62 +4,74 @@ from buildbot.plugins import util
 codebases = {
     'benchbuild': {
         'repository': 'https://github.com/PolyJIT/benchbuild.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'polli': {
         'repository': 'https://github.com/PolyJIT/polli.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'polli-sb': {
         'repository': 'https://github.com/PolyJIT/PolyJIT.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'polly': {
         'repository': 'https://github.com/PolyJIT/polly.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'llvm': {
         'repository': 'https://github.com/PolyJIT/llvm.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'clang': {
         'repository': 'https://github.com/PolyJIT/clang.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'compiler-rt': {
         'repository': 'http://llvm.org/git/compiler-rt.git',
-        'branch': ['master', 'release_40'],
+        'branches': ['master', 'release_40'],
+        'branch': 'master',
         'revision': None
     },
     'openmp': {
         'repository': 'http://llvm.org/git/openmp.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'stats': {
         'repository': 'https://github.com/simbuerg/pprof-stats.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'isl': {
         'repository': 'https://github.com/simbuerg/isl.git',
-        'branch': ['isl-0.16.1-cpp', 'master'],
+        'branches': ['master, isl-0.16.1-cpp'],
+        'branch': 'master',
         'revision': None
     },
     'isl-cpp': {
         'repository': 'https://github.com/simbuerg/isl-cpp.git',
-        'branch': ['master'],
+        'branches': ['master'],
+        'branch': 'master',
         'revision': None
     },
     'likwid': {
         'repository': 'https://github.com/RRZE-HPC/likwid.git',
-        'branch': ['v4.1'],
+        'branches': ['v4.1'],
+        'branch': 'v4.1',
         'revision': None
     },
     'vara': {
@@ -111,37 +123,37 @@ def configure(c):
     c['change_source'] = [
         changes.GitPoller(repourl=codebases["benchbuild"]["repository"],
                           workdir='gitpoller-benchbuild',
-                          branches=codebases["benchbuild"]["branch"],
+                          branches=codebases["benchbuild"]["branches"],
                           project="polyjit",
                           pollinterval=5 * 60),
         changes.GitPoller(repourl=codebases["polli"]["repository"],
                           workdir='gitpoller-polli',
-                          branches=codebases["polli"]["branch"],
+                          branches=codebases["polli"]["branches"],
                           project="polyjit",
                           pollinterval=5 * 60),
         changes.GitPoller(repourl=codebases["polli-sb"]["repository"],
                           workdir='gitpoller-polli-sb',
-                          branches=codebases["polli-sb"]["branch"],
+                          branches=codebases["polli-sb"]["branches"],
                           project="polyjit",
                           pollinterval=5 * 60),
         changes.GitPoller(repourl=codebases["llvm"]["repository"],
                           workdir='gitpoller-llvm',
-                          branches=codebases["llvm"]["branch"],
+                          branches=codebases["llvm"]["branches"],
                           project="llvm",
                           pollinterval=5 * 60),
         changes.GitPoller(repourl=codebases["clang"]["repository"],
                           workdir='gitpoller-clang',
-                          branches=codebases["clang"]["branch"],
+                          branches=codebases["clang"]["branches"],
                           project="llvm",
                           pollinterval=5 * 60),
         changes.GitPoller(repourl=codebases["polly"]["repository"],
                           workdir='gitpoller-polly',
-                          branches=codebases["polly"]["branch"],
+                          branches=codebases["polly"]["branches"],
                           project="polyjit",
                           pollinterval=5 * 60),
         changes.GitPoller(repourl=codebases["stats"]["repository"],
                           workdir='gitpoller-stats',
-                          branches=codebases["stats"]["branch"],
+                          branches=codebases["stats"]["branches"],
                           project="polyjit",
                           pollinterval=5 * 60),
         changes.GitPoller(repourl=codebases["vara"]["repository"],
