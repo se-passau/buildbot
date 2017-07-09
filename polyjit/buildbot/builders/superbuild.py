a603f14bbf7fa7565024b70a2b63aab2ab2fe8f7
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-07-10 01:53:19 +0200

    wipe builddir

--- polyjit/buildbot/builders/superbuild.py
+++ polyjit/buildbot/builders/superbuild.py
@@ -29,7 +29,7 @@ def configure(c):
         define("UCHROOT_SUPERBUILD_ROOT", "/mnt/polli-sb"),
         define("POLYJIT_DEFAULT_BRANCH", "master"),
 
-        cmd('rm', '-r', P("SUPERBUILD_ROOT")),
+        cmd('rm', '-r', P("builddir")),
         git('polli-sb', 'master', codebases, workdir=P("SUPERBUILD_ROOT"),
             mode="full", method="fresh"),
         cmd(ip('%(prop:cmake_prefix)s/bin/cmake'), P("SUPERBUILD_ROOT"),
